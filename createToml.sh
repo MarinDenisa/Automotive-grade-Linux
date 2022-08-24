5651aa149e250835a5432d7e7fdb30a07b8cab27
Alexandru Raducu <alexandru.raducu.03@gmail.com>
2022-08-24 06:41:25 -0700

    Refactor script to be used with arguments
    Signed-off-by: Alexandru Raducu <alexandru.raducu.03@gmail.com>

--- createToml.sh
+++ createToml.sh
@@ -3,43 +3,67 @@
 launch-update() {
 	echo "${HOME}/.cargo/bin/ota update create -t ./$file"
 	update_id=$(${HOME}/.cargo/bin/ota update create -t ./$file)
-    update_id="${update_id%\"}"
-    update_id="${update_id#\"}"
-    device_id=$(ls ${HOME}/workspace/ota-lith/ota-ce-gen/devices | head -1)
-    echo "device ID - $device_id"
-    echo "update ID - $update_id"
-    echo "${HOME}/.cargo/bin/ota update launch --update $update_id --device $device_id"
-    ${HOME}/.cargo/bin/ota update launch --update $update_id --device $device_id
+	update_id="${update_id%\"}"
+	update_id="${update_id#\"}"
+	device_id=$(ls ${HOME}/workspace/ota-lith/ota-ce-gen/devices | head -1)
+	echo "device ID - $device_id"
+	echo "update ID - $update_id"
+	echo "${HOME}/.cargo/bin/ota update launch --update $update_id --device $device_id"
+	${HOME}/.cargo/bin/ota update launch --update $update_id --device $device_id
 }
 
-if [ $# -eq 0 ]
-then
-    echo "Error: give a file as parameter"
-else  
-	touch output
-	echo "$HOME/.cargo/bin/ota package add -n $1 -v 2 -p ./$1 -b -h ota-ce-device > output"
-	$HOME/.cargo/bin/ota package add -n $1 -v 2 -p ./$1 -b -h ota-ce-device > output
+Help() {
+	echo "Help me"
+}
 
-	file="$1.toml"
-	jsonPath=".signed.targets.\"$1-2\""
-	touch $file
+generate-tome() {
+	if [ -z ${fileName+x} ]
+	then
+		echo "Error: give a file as parameter"
+	else  
+		touch output
+		echo "$HOME/.cargo/bin/ota package add -n $fileName -v 2 -p ./$fileName -b -h ota-ce-device > output"
+		$HOME/.cargo/bin/ota package add -n $fileName -v 2 -p ./$fileName -b -h ota-ce-device > output
 
-	echo "# specify each hardware identifier in square brackets" > $file
-	echo "[ota-ce-device]" >> $file
+		file="$fileName.toml"
+		jsonPath=".signed.targets.\"$fileName-2\""
+		touch $file
 
-	echo "target_format = $(jq "$jsonPath.custom.targetFormat" output)" >> $file
-	echo -e "generate_dif = true\n" >> $file
+		echo "# specify each hardware identifier in square brackets" > $file
+		echo "[ota-ce-device]" >> $file
 
-	echo "# required metadata specifying update target" >> $file
-	echo "[ota-ce-device.to]" >> $file
-	echo "name = \"$1\"" >> $file
-	echo "version = $(jq "$jsonPath.custom.version" output)" >> $file
-	echo "length = $(jq "$jsonPath.length" output)" >> $file
-	
-	hashMethod=$(jq "$jsonPath.hashes" output | jq 'keys'[0])
-	echo "hash = $(jq "$jsonPath.hashes.$hashMethod" output)" >> $file
-	echo "method = ${hashMethod}" >> $file
-	
-	launch-update	
-fi
+		echo "target_format = $(jq "$jsonPath.custom.targetFormat" output)" >> $file
+		echo -e "generate_dif = true\n" >> $file
 
+		echo "# required metadata specifying update target" >> $file
+		echo "[ota-ce-device.to]" >> $file
+		echo "name = \"$fileName\"" >> $file
+		echo "version = $(jq "$jsonPath.custom.version" output)" >> $file
+		echo "length = $(jq "$jsonPath.length" output)" >> $file
+		
+		hashMethod=$(jq "$jsonPath.hashes" output | jq 'keys'[0])
+		echo "hash = $(jq "$jsonPath.hashes.$hashMethod" output)" >> $file
+		echo "method = ${hashMethod}" >> $file	
+	fi
+}
+
+fileName=null
+
+while getopts ":lhn:" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      n) # Enter a name
+         fileName=$OPTARG
+         generate-tome;;
+	  l) #launch update
+		 launch-update;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+
+echo "### END OF SCRIPT ###"
